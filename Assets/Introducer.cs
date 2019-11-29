6f7063752b7f2c786085485dc70ffc2df5d9fa3a
Quentin Désert <quentin.desert@gmail.com>
2019-11-29 13:15:58 -0600

    Added Introducer script to Car 3

---
+++ Assets/Introducer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start() {
+        print(10 / 2);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
